c5f93c0a9bffcf09979b4b8f745b4b7c0857ee3e
MatheusRph <matheus.rodolpho16@gmail.com>
2024-06-05 17:10:43 -0300

    Add Styles, containers, grids, main, etc...

--- new-frontend/src/app/page.jsx
+++ new-frontend/src/app/page.jsx
@@ -1,22 +1,47 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+import React from 'react';
+import Container from "@/components/Container/Container";
+import Header from "@/components/Header/header";
+
 
 export default function Home() {
   return (
     <>
-    {/* Começo da página */}
-      <header>
-      
-      {/* Navbar */}
+      {/* Começo da página */}
+      <Header>
+        <Navbar></Navbar>
+      </Header>
 
-      </header>
+      <Container>
+        {/* conteúdo principal */}
+        <main class={`rounded-4 flex-column ${styles.main}`}>
+          {/* seria um layout igual o do login com as turmas. */}
+          <div class={`col-12 jomhuria-regular ${styles.title}`}>
+            <h1 class={`${styles.titleHeader}`}>Turmas</h1>
+          </div>
+          <div class={`m-2 col-10 container-turmas ${styles.container}`}>
 
-      {/* conteúdo principal */}
-      <main>
-        {/* seria um layout igual o do login com as turmas. */}
-      </main>
 
 
+
+            <div class={`card ${styles.card}`}>
+              {/* <img src="img_avatar.png" alt="Avatar" style="width:100%"> */}
+              <div class="container">
+                <h2><b>John Doe</b></h2>
+                <p>Architect & Engineer</p>
+              </div>
+            </div>
+
+
+          </div>
+        </main>
+      </Container>
+
+      <footer >
+        {/* Rodapé */}
+      </footer>
     </>
   );
 }
