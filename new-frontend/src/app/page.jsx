d1d3e071eceb2909cf270edf71af0497a64e8761
Theus <144968520+MatheusRph@users.noreply.github.com>
2024-06-05 00:46:28 +0000

    New-FrontEnd

---
+++ new-frontend/src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+    {/* Começo da página */}
+      <header>
+      
+      {/* Navbar */}
+
+      </header>
+
+      {/* conteúdo principal */}
+      <main>
+        {/* seria um layout igual o do login com as turmas. */}
+      </main>
+
+
+    </>
+  );
+}
