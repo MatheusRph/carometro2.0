f08fbf119af0d0d6235b7ad533c1c1f8a41fd4db
MatheusRph <matheus.rodolpho16@gmail.com>
2024-06-05 13:36:15 -0300

    New pastes

--- front-end/src/components/yourpaste.js
+++ front-end/src/components/yourpaste.js
@@ -1,4 +1,6 @@
-import { redirect } from 'next/dist/server/api-utils';
+"use client"
+
+import React, { useEffect, useState } from 'react';
 import './styles/yourpaste.css';
 import Image from 'next/image';
 
@@ -6,6 +8,17 @@ export default function Yourpastes() {
     const redirectToTurmas = () => {
         window.location.assign('/turmas');
     };
+
+    const [turmas, setTurmas] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/turmas')
+            .then(response => response.json())
+            .then(data => setTurmas(data))
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }, []);
 
     return (
         <div className="container col-8 text-center yourpastes d-flex">
@@ -17,70 +30,31 @@ export default function Yourpastes() {
                     </div>
                 </div>
                 <div className="col div-class col-12 d-flex flex-wrap justify-content-center">
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class idev'>
-                            <h2>Idev-1</h2>
-                        </div>
-                    </button>
 
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class idev'>
-                            <h2>Idev-2</h2>
-                        </div>
-                    </button>
+                    {turmas ? (
+                        <>
+                            {turmas.map(turma => (
+                                <button key={turma.idTurmas} className="" onClick={redirectToTurmas} style={{ border: 'none', background: 'none', cursor: 'pointer' }}>
+                                    <div className='class idev'>
+                                        <h2>{turma.codigo}</h2>
+                                    </div>
+                                </button>
+                            ))}
+                        </>
 
-                    <button className="" onClick={redirectToTurmas}
+                    ) : (
+                        <></>
+                    )}
+
+                    {/* <button className="" onClick={redirectToTurmas}
                         style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
                         <div className='class idev'>
                             <h2>Idev-3</h2>
                         </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-1</h2>
-                        </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-2</h2>
-                        </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-3</h2>
-                        </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-3</h2>
-                        </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-3</h2>
-                        </div>
-                    </button>
-
-                    <button className="" onClick={redirectToTurmas}
-                        style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}>
-                        <div className='class elec-mec'>
-                            <h2>Elec/Mec-3</h2>
-                        </div>
-                    </button>
+                    </button> */}
+                    
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
