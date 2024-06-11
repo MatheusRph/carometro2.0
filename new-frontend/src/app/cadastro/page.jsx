3ff703cf41dddef3f71bd44a1a4c61c04e0d9774
Theus <144968520+MatheusRph@users.noreply.github.com>
2024-06-11 19:50:31 +0000

    Add use effect, para controlar avanço de forms

--- new-frontend/src/app/cadastro/page.jsx
+++ new-frontend/src/app/cadastro/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Navbar from '@/components/Navbar/Navbar'
 import styles from './page.module.css'
 import Container from '@/components/Container/Container'
@@ -5,65 +7,53 @@ import Header from '@/components/Header/Header'
 import Form from '@/components/Form/Form'
 import Input from '@/components/Input/Input'
 import Button from '@/components/Button/Button'
+import { useState } from 'react'
 
 
 export default function Cadastro() {
 
     const [status, setStatus] = useState(0);
+    // Cidade
+    // Bairro
+    // Telefone
+    // Cep tipo de usuário
+    // foto
 
     return (
         <>
-
-
-            {status === 0 ? (
-                <main className={`rounded-4 flex-column ${styles.main}`}>
-                    <Form>
-
-                        <div className="mb-3">
-                            <h3 className="form-label">Cadastro</h3>
-                            <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
-                        </div>
-                        <Input label={'Nome do usuário'} type={'text'} placeholder={'Insira o nome do usuário'} className={`form-control ${styles.input}`}></Input>
-                        <Input label="CPF" type={"text"} placeholder={'Insira o CPF'} className={`${styles.input}`}></Input>
-                        <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
-
-                    </Form>
-                </main>
-            ) : status === 1 ? (
-                <Cadas2 user={status} setUser={setStatus} />
-            ) : (null)}
-
-
             <Header>
                 <Navbar></Navbar>
             </Header>
 
             <Container>
-                <main className={`rounded-4 flex-column ${styles.main}`}>
-                    <Form>
 
-                        <div className="mb-3">
-                            <h3 className="form-label">Cadastro</h3>
-                            <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
-                        </div>
-                        <Input label={'Nome do usuário'} type={'text'} placeholder={'Insira o nome do usuário'} className={`form-control ${styles.input}`}></Input>
-                        <Input label="CPF" type={"text"} placeholder={'Insira o CPF'} className={`${styles.input}`}></Input>
-                        <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
+                {status === 0 ? (
+                    <main className={`rounded-4 flex-column ${styles.main}`}>
+                        <Form>
 
-                    </Form>
-                </main>
+                            <div className="mb-3">
+                                <h3 className="form-label">Cadastro</h3>
+                                <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
+                            </div>
+                            <Input label={'Nome do usuário'} type={'text'} placeholder={'Insira o nome do usuário'} className={`form-control ${styles.input}`}></Input>
+                            <Input label="CPF" type={"text"} placeholder={'Insira o CPF'} className={`${styles.input}`}></Input>
+                            <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
 
-                <main className={`rounded-4 flex-column ${styles.main}`}>
-                    <Form>
-                        <div className='mb-3'>
-                            <h3 className='form-label'>Cadastro</h3>
-                            <div id="helps" className='form-text'>Bem vindo(a) ao Portal Online do SENAI-SP</div>
-                        </div>
-                        <Input label={'Email'} type={'email'} placeholder={'Digite o email'} className={`${styles.input}`}></Input>
-                        <Input label={'Senha'} type={'password'} placeholder={'Digite a senha'} className={`${styles.input}`}></Input>
-                        <Button type={'submit'} className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
-                    </Form>
-                </main>
+                        </Form>
+                    </main>
+                ) : status === 1 ? (
+                    <main className={`rounded-4 flex-column ${styles.main}`}>
+                        <Form>
+                            <div className='mb-3'>
+                                <h3 className='form-label'>Cadastro</h3>
+                                <div id="helps" className='form-text'>Bem vindo(a) ao Portal Online do SENAI-SP</div>
+                            </div>
+                            <Input label={'Email'} type={'email'} placeholder={'Digite o email'} className={`${styles.input}`}></Input>
+                            <Input label={'Senha'} type={'password'} placeholder={'Digite a senha'} className={`${styles.input}`}></Input>
+                            <Button type={'submit'} className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
+                        </Form>
+                    </main>
+                ) : (null)}
             </Container>
         </>
     )
--- new-frontend/src/app/login/page.jsx
+++ new-frontend/src/app/login/page.jsx
@@ -1,14 +1,20 @@
+'use client'
+
 import Container from "@/components/Container/Container";
 import Header from "@/components/Header/Header";
 import Navbar from "@/components/Navbar/Navbar";
 import styles from "./page.module.css";
-import "./style.css"
 import Form from "@/components/Form/Form";
 import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
 
+import { useState } from "react";
+
 
 export default function Login() {
+
+    const [status, setStatus] = useState(0);
+
     return (
         <>
             <Header>
@@ -16,18 +22,37 @@ export default function Login() {
             </Header>
 
             <Container>
-                <main className={`rounded-4 flex-column ${styles.main}`}>
-                    <Form>
-                        <div className="interaction">
-                            <div className="mb-3">
-                                <h3 className="form-label">Login</h3>
-                                <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
-                            </div>
-                            <Input label="Usuário" type={"text"} placeholder={'Insira seu CPF'} className={`form-control ${styles.input}`}></Input>
-                            <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
-                        </div>
-                    </Form>
-                </main>
+                {status === 0 ? (
+                    <>
+                        <main className={`rounded-4 flex-column ${styles.main}`}>
+                            <Form>
+
+                                <div className="mb-3">
+                                    <h3 className="form-label">Login</h3>
+                                    <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
+                                </div>
+                                <Input label="Usuário" type={"text"} placeholder={'Insira seu CPF'} className={`${styles.input}`}></Input>
+                                <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Avançar</Button>
+
+                            </Form>
+                        </main>
+                    </>
+                ) : (
+                    <>
+                        <main className={`rounded-4 flex-column ${styles.main}`}>
+                            <Form>
+
+                                <div className="mb-3">
+                                    <h3 className="form-label">Login</h3>
+                                    <div id="helps" className="form-text">Bem-vindo(a) ao Portal Online SENAI-SP</div>
+                                </div>
+                                <Input label="Senha" type={"password"} placeholder={'Insira sua senha'} className={`${styles.input}`}></Input>
+                                <Button type="submit" className={`btn-danger w-100 ${styles.button}`}>Entrar</Button>
+
+                            </Form>
+                        </main>
+                    </>
+                )}
             </Container>
 
         </>
